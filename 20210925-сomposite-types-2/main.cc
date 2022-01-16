233c0b7045d9b6889d3436e68e4ead5f79dfcc21
Gorbunov Nikita <71311392+NiktiaGorbunov@users.noreply.github.com>
2022-01-16 18:33:41 +0500

    База данных рабочих

---
+++ 20210925-сomposite-types-2/main.cc
@@ -0,0 +1,220 @@
+/*
+ * main.cc
+ *
+ *  Created on: 27 нояб. 2021 г.
+ *      Author: Никита
+ */
+
+/* ЗАДАЧА
+ * Есть список сотрудников.
+ * У сотрудников есть ФИО, должность, зарплата.
+ * Вывести список сотрудников в виде таблицы.
+ *
+ *
+ */
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+const char *DB_FILE_NAME = "empl_roster.db";
+
+
+struct Employee
+{
+	char name[21];
+	char job [16];
+	int salary;
+};
+
+void print_table_header(){
+	std::cout <<
+			"  №  |     Employee       |       Job     |Salary|\n"
+			"-----+--------------------+---------------+------|\n";
+
+}
+
+void print_table_row(const Employee &empl, int row){
+	std::cout <<
+		std::setw(4) <<std::right << row << " |" <<
+		std::setw(20) << std::left  << empl.name   << "|" <<
+		std::setw(15) << std::left  << empl.job    << "|"<<
+		std::setw(6)  << std::right << empl.salary << "|"<<
+		std::endl;
+
+}
+
+void read_file(const char*filename, std::vector <Employee> &list)
+{
+	list.clear();
+
+	std::ifstream data_file {filename, std::ios::binary};
+	if (not data_file) return;
+
+	Employee empl;
+
+	while(data_file.read( reinterpret_cast<char*>(&empl), sizeof(empl)))
+		list.push_back(empl);
+
+	data_file.close();
+
+}
+
+void write_file(const char*filename, std::vector <Employee> &list){
+
+	std::ofstream data_file{filename, std::ios::binary };
+
+	for(auto &&e : list) data_file.write(reinterpret_cast<char*>(&e), sizeof(e));
+
+	data_file.close();
+
+}
+
+void input_employee(std::vector <Employee> &list){
+
+	Employee empl;
+
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	std::cout << "Введите имя сотрудника: ";
+	std::cin.getline(empl.name, sizeof(empl.name));
+
+
+	std::cout << "Введите должность     : ";
+	std::cin.getline(empl.job, sizeof(empl.job));
+
+
+	std::cout << "Введите зарплату      : ";
+	std::cin >> empl.salary;
+
+	list.push_back(empl);
+
+
+}
+
+void change_employee(std::vector <Employee> &list){
+	Employee empl;
+
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+
+
+	std::cout << "Введите номер записи:\n--->";
+
+	long n;
+	std::cin >> n;
+
+	if ( n < 0 || n > list.size()){
+		std::cout << "Такого номера нет в базе!" << std::endl;
+	}else{
+		list.erase(list.begin() + n);
+
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+		std::cout << "Введите имя сотрудника: ";
+		std::cin.getline(empl.name, sizeof(empl.name));
+
+		std::cout << "Введите должность     : ";
+		std::cin.getline(empl.job, sizeof(empl.job));
+
+		std::cout << "Введите зарплату      : ";
+		std::cin >> empl.salary;
+
+		list.emplace(list.begin()+n, empl);
+	}
+
+
+
+}
+
+void delte_employee(std::vector <Employee> &list){
+
+
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	std::cout << "Введите номер записи:\n--->";
+
+	long n;
+	std::cin >> n;
+
+	if ( n < 0 || n > list.size()){
+		std::cout << "Такого номера нет в базе!" << std::endl;
+	}else{
+		list.erase(list.begin() + n);
+	}
+
+}
+
+int main(){
+
+	std::vector <Employee> empl_roster;
+
+
+/*
+ *	удаление записи
+ *	empl_roster.erase(empl_roster.begin() + 0);\
+ *	Начало + номер записи
+ */
+
+	read_file(DB_FILE_NAME, empl_roster);
+
+	int choice;
+
+	do{
+menu:
+		std::cout <<
+				"\n\n"
+				"Действия над списокм:\n"
+				"---------------------\n"
+				"1. Вывести список\n"
+				"2. Добавить сотрудника\n"
+				"3. Изменить запись\n"
+				"4. Удалить запись\n"
+				"\n"
+				"0. Выход\n"
+				"---->";
+
+		if(not(std::cin >> choice)){
+			if(std::cin.eof()){
+				std::cerr << "ошбика вводы-вывода" << std::endl;
+				return 1;
+			}
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			goto menu;
+		}
+
+		int i = 0;
+
+		switch(choice){
+		case 0:
+			std::cout <<"До свидания!" << std::endl;
+			break;
+		case 1:
+			print_table_header();
+			for(auto &&e : empl_roster)
+					print_table_row(e, i++);
+			break;
+		case 2:
+			input_employee(empl_roster);
+			break;
+		case 3:
+			change_employee(empl_roster);
+			break;
+		case 4:
+			delte_employee(empl_roster);
+			break;
+
+		default:
+			std::cout << "Нет такого варианта!" << std::endl;
+
+		}
+	}while (choice != 0);
+
+	write_file(DB_FILE_NAME, empl_roster);
+
+
+	return 0;
+}
